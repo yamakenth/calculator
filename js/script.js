9af4a8b0d567ae06570ff497602a2e5ec20e07fe
Ken Yamada <yamakenth@gmail.com>
2021-12-05 21:13:35 +0700

    Add comments to operator functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
+// add two numbers 
+// take in two numbers 
+// return sum
 function add(a, b) {
   return a + b;
 }
 
+// subtract second param from first param 
+// take in two numbers 
+// return difference 
 function subtract(a, b) {
   return a - b;
 }
 
+// mulitply two numbers 
+// take in two numbers 
+// return product 
 function multiply(a, b) {
   return a * b;
 }
 
+// divide first param by second param
+// take in two numbers 
+// return product or '#DIV/0!'
 function divide(a, b) {
   return (b === 0) ? '#DIV/0!' : a / b;
 }
