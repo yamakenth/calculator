5169141bafbe4abb53cdd52473d88547b49a4297
Ken Yamada <yamakenth@gmail.com>
2021-12-06 17:15:59 +0700

    Change button color when hover

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,14 @@
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
+  // change scale
   button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering'));
   button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering'));
+  // change color 
+  if (button.className === 'clear-button') {
+    button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-clear-button'));
+    button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-clear-button'));
+  } else if (button.className === 'operand-button') {
+    button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-operand-button'));
+    button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-operand-button'));
+  }
 });
