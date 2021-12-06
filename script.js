85f5bf83cf3f5f17dadbee85b3223a4a8c84368b
Ken Yamada <yamakenth@gmail.com>
2021-12-06 22:34:05 +0700

    Add math functions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello world');
+// add two numbers 
+// take in two numbers 
+// return sum
+function add(a, b) {
+  return a + b;
+}
+
+// subtract second param from first param 
+// take in two numbers 
+// return difference 
+function subtract(a, b) {
+  return a - b;
+}
+
+// mulitply two numbers 
+// take in two numbers 
+// return product 
+function multiply(a, b) {
+  return a * b;
+}
+
+// divide first param by second param (check for div 0 later)
+// take in two numbers 
+// return product
+function divide(a, b) {
+  return a / b;
+}
