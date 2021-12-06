164fe8c5419a19707a5e41e1cc5a794ef9b63a8c
Ken Yamada <yamakenth@gmail.com>
2021-12-06 20:17:41 +0700

    Change class names

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,32 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-  // change scale
+// change scale on hover 
+const styleAllButtons = document.querySelectorAll('button');
+styleAllButtons.forEach((button) => {
   button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering'));
   button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering'));
-  // change color 
-  if (button.className === 'clear-button') {
-    button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-clear-button'));
-    button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-clear-button'));
-  } else if (button.className === 'operand-button' || button.className === 'equal-button') {
-    button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-operand-button'));
-    button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-operand-button'));
-  }
+});
+
+// add red to clear buttons 
+const styleClearButtons = document.querySelectorAll('.clear-button');
+styleClearButtons.forEach((button) => {
+  button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-clear-button'));
+  button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-clear-button'));
+});
+
+// add orange to operand buttons 
+const styleOperandButtons = document.querySelectorAll('.operand-button');
+styleOperandButtons.forEach((button) => {
+  button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-operand-button'));
+  button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-operand-button'));
+});
+
+// add orange to equal button
+const styleEqualButton = document.querySelector('.equal-button');
+styleEqualButton.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-operand-button'));
+styleEqualButton.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-operand-button'));
+
+// add lighter grey to number buttons 
+const styleNumberButtons = document.querySelectorAll('.number-button');
+styleNumberButtons.forEach((button) => {
+  button.addEventListener('mouseenter', (e) => e.target.classList.add('hovering-number-button'));
+  button.addEventListener('mouseleave', (e) => e.target.classList.remove('hovering-number-button'));
 });
